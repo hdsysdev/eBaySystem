7d78075e3019100846e71b12e2169b22667587d5
Hubert <hdudowicz@gmail.com>
2018-08-02 13:29:41 +0100

    Second commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,70 @@
 package sample;
 
+import com.opencsv.CSVWriter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
+import javax.swing.*;
+import java.io.*;
+import java.sql.*;
 
 public class Controller {
 
     @FXML
-    public void importNewPrices(ActionEvent event) {
+    public void importNewPrices(ActionEvent event) throws FileNotFoundException {
+        String connectionUrl = "jdbc:sqlserver://DESIGNER1:1433;databaseName=INTRANET;user=intranet;password=bathcountry110";
 
+        String SQL = "";
+
+
+        BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("C:\\Users\\hubert\\IdeaProjects\\eBaySystem\\src\\sample\\Price Pull.sql")
+        );
+
+        String thisLine, sqlQuery;
+        try {
+            sqlQuery = "";
+            while ((thisLine = bufferedReader.readLine()) != null)
+            {
+                //Skip comments and empty lines
+                if(thisLine.length() > 0 && thisLine.charAt(0) == '-' || thisLine.length() == 0 )
+                    continue;
+                sqlQuery = sqlQuery + " " + thisLine;
+                //If one command complete
+                if(sqlQuery.charAt(sqlQuery.length() - 1) == ';') {
+                    sqlQuery = sqlQuery.replace(';' , ' '); //Remove the ; since jdbc complains
+                    try {
+                        // stmt.execute(sqlQuery);
+                    }
+                    catch(Exception ex) {
+                        JOptionPane.showMessageDialog(null, "Error Creating the SQL Database : " + ex.getMessage());
+                    }
+                    sqlQuery = "";
+                }
+            }
+            SQL = sqlQuery;
+        }
+        catch(IOException ex) {
+
+        }
+
+
+        try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            Date todayWithZeroTime = new java.sql.Date(System.currentTimeMillis());
+            // Iterate through the data in the result set and display it.
+            while (rs.next()) {
+                CSVWriter csvWriter = new CSVWriter(new FileWriter("Price Pull_" + todayWithZeroTime.toString() + ".csv"));
+                csvWriter.writeAll(rs, true);
+                System.out.println("Finished");
+            }
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import com.opencsv.CSVWriter;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,14 +8,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import net.sourceforge.jtds.jdbc.DateTime;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.sql.*;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class Main extends Application {
 
@@ -26,55 +28,6 @@ public class Main extends Application {
         primaryStage.show();
 
 
-        String connectionUrl = "jdbc:sqlserver://DESIGNER1:1433;databaseName=INTRANET;user=intranet;password=bathcountry110";
-
-        String SQL = "";
-
-
-        BufferedReader bufferedReader = new BufferedReader(
-                new FileReader("C:\\Users\\hubert\\IdeaProjects\\eBaySystem\\src\\sample\\Price Pull.sql")
-        );
-
-        String thisLine, sqlQuery;
-        try {
-            sqlQuery = "";
-            while ((thisLine = bufferedReader.readLine()) != null)
-            {
-                //Skip comments and empty lines
-                if(thisLine.length() > 0 && thisLine.charAt(0) == '-' || thisLine.length() == 0 )
-                    continue;
-                sqlQuery = sqlQuery + " " + thisLine;
-                //If one command complete
-                if(sqlQuery.charAt(sqlQuery.length() - 1) == ';') {
-                    sqlQuery = sqlQuery.replace(';' , ' '); //Remove the ; since jdbc complains
-                    try {
-                        // stmt.execute(sqlQuery);
-                    }
-                    catch(Exception ex) {
-                        JOptionPane.showMessageDialog(null, "Error Creating the SQL Database : " + ex.getMessage());
-                    }
-                    sqlQuery = "";
-                }
-            }
-            SQL = sqlQuery;
-        }
-        catch(IOException ex) {
-        }
-
-
-        try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
-            ResultSet rs = stmt.executeQuery(SQL);
-
-            // Iterate through the data in the result set and display it.
-            while (rs.next()) {
-                System.out.println(rs);
-
-            }
-        }
-        // Handle any errors that may have occurred.
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
 
 
     }
